62d3642f3472f276d1393bbdb23f67d4c5c53a88
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-02 21:54:48 -1000

    server is created w/ hbs

--- server.js
+++ server.js
@@ -1,4 +1,19 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
 
+const hbs = handlebars.create({
+  extname: '.hbs',
+  defaultlayout: 'app'
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+
+app.use('/', (req, res)=>{
+  res.render('index');
+});
+
+module.exports = app;
