0dd24c30418d23060b7c89c7c7ab6a4e97f98b68
jonathonlaylo <jonathon.laylo@gmail.com>
2017-02-02 20:41:17 -1000

    the cases of edge is working with the module.exports. Also created the sever

--- bubble-sort.js
+++ bubble-sort.js
@@ -1,7 +1,28 @@
 /*jshint esversion: 6 */
 
-let myArray = [5,1,4,2,8];
+// let myArray = [9,2,5,6,4,3,7,10,1,8];
 
-function bubbleSort(argument) {
-  // body...
+function swap(myArray, i, j) {
+  var temp = myArray[i];
+  myArray[i] = myArray[j];
+  myArray[j] = temp;
 }
+
+function bubbleSort(myArray) {
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < myArray.length; i++) {
+      if (myArray[i] !== undefined && myArray[i+1] !== undefined && myArray[i] > myArray[i+1]) {
+        swap(myArray, i, i+1);
+        // console.log('myArray', myArray);
+        swapped = true;
+      }
+    }
+  } while(swapped);
+  return myArray;
+}
+
+// console.log(bubbleSort(myArray));
+
+module.exports =  bubbleSort;
--- test/test-script.js
+++ test/test-script.js
@@ -6,19 +6,37 @@ chai.should();
 
 const bubbleSort = require('../bubble-sort.js');
 
-describe('myArray', () => {
-  let myArray = bubbleSort;
+describe('The Cases of Edge', () => {
 
-  it('should be a class', ()=>{
-    myArray.should.be.a.function
+  it('should be a function', ()=>{
+    bubbleSort.should.be.a('function');
   });
 
-  it('shoud be a integer', () => {
-    myArray.should.have.property(integer);
+  it('shoud be not be an array', () => {
+    bubbleSort.should.not.be.an('array');
   });
-});
 
-describe('', () => {
-  //TEST HEEEERE
+  it('should not be a boolean',()=>{
+    bubbleSort.should.not.be.a('boolean');
+  });
 
+  it('should not be a string', ()=>{
+    bubbleSort.should.not.be.a('string');
+  });
+
+  it('should not be null', ()=>{
+    bubbleSort.should.not.equal('null');
+  });
+
+  it('should return an array', ()=>{
+    bubbleSort([9,2,5,6,4,3,7,10,1,8]).should.be.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return an object', ()=>{
+    bubbleSort([{}, {}, {},{}]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
+
+  it('should not return the same numbers', ()=>{
+    bubbleSort([1,1,1,1,1,1,1]).should.not.deep.equal([1,2,3,4,5,6,7,8,9,10]);
+  });
 });
